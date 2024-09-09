20bfdcdcd4e39699efa813c9afbeab4e6d5f0d71
NexusQuile <104992166+NexusQuile@users.noreply.github.com>
2024-09-09 14:31:43 +0100

    Move pitch angles to the edge
    Move pitch angles to the edge

--- content/scripts/vehicle_hud.lua
+++ content/scripts/vehicle_hud.lua
@@ -1829,7 +1829,7 @@ function render_flight_hud(screen_w, screen_h, is_render_center, vehicle)
     end
 
     if is_render_center then
-        render_artificial_horizion(screen_w, screen_h, hud_pos, vec2(hud_size:x() - 40, hud_size:y()), vehicle, col)
+		render_artificial_horizion(screen_w, screen_h, hud_pos, vec2(hud_size:x() - 40, hud_size:y()), vehicle, col)
     end
 
     if g_is_render_compass then
@@ -2020,7 +2020,7 @@ end
 
 function render_artificial_horizion(screen_w, screen_h, pos, size, vehicle, col)
     update_ui_push_clip(pos:x() - size:x() / 2, pos:y() - size:y() / 2, size:x(), size:y())
-
+	
     local scale = 1.5
 
     local forward = update_get_camera_forward()
@@ -2053,20 +2053,32 @@ function render_artificial_horizion(screen_w, screen_h, pos, size, vehicle, col)
     local angle_step = angle_step_deg / 180 * math.pi
     local steps = math.floor(math.pi * 0.5 / angle_step)
 	local angle_width = 20
+	local hoz_width = 41
 
     for i = 1, steps do
         projected_forward = vec3(position:x() + forward_xz:x() * project_dist, position:y() + math.tan(i * angle_step) * project_dist, position:z() + forward_xz:z() * project_dist)
         horizon = artificial_horizon_to_screen(screen_w, screen_h, pos, scale, update_world_to_screen(projected_forward))
    		
+		
         if i ~= steps then
             update_ui_image_rot(horizon:x(), horizon:y(), atlas_icons.hud_horizon_high, col, roll)
-			update_ui_text(horizon:x()-angle_width/2, horizon:y()-5, math.floor(i*angle_step_deg), 20, 1, col, 0)
+			--update_ui_text(horizon:x()-angle_width/2, horizon:y()-5, math.floor(i*angle_step_deg), 20, 1, col, 0)
+			
+			local hoz_width = 38
+			update_ui_text(horizon:x()-angle_width/2-math.floor(hoz_width*math.cos(roll)), horizon:y()-5-math.floor(hoz_width*math.sin(roll)), math.floor(i*angle_step_deg), 20, 1, col, 0)
+			update_ui_text(horizon:x()-angle_width/2+math.floor(hoz_width*math.cos(roll))+1, horizon:y()-5+math.floor(hoz_width*math.sin(roll)), math.floor(i*angle_step_deg), 20, 1, col, 0)
         end
 
         projected_forward = vec3(position:x() + forward_xz:x() * project_dist, position:y() - math.tan(i * angle_step) * project_dist, position:z() + forward_xz:z() * project_dist)
         horizon = artificial_horizon_to_screen(screen_w, screen_h, pos, scale, update_world_to_screen(projected_forward))
         update_ui_image_rot(horizon:x(), horizon:y(), atlas_icons.hud_horizon_low, col, roll)
-		update_ui_text(horizon:x()-angle_width/2, horizon:y()-3, math.floor(-i*angle_step_deg), 20, 1, col, 0)
+		
+		
+		--update_ui_text(horizon:x()-angle_width/2, horizon:y()-3, math.floor(-i*angle_step_deg), 20, 1, col, 0)
+		
+		local hoz_width = 41
+		update_ui_text(horizon:x()-angle_width/2-math.floor(hoz_width*math.cos(roll)), horizon:y()-3-math.floor(hoz_width*math.sin(roll)), math.floor(-i*angle_step_deg), 20, 1, col, 0)
+		update_ui_text(horizon:x()-angle_width/2+math.floor(hoz_width*math.cos(roll))+1, horizon:y()-3+math.floor(hoz_width*math.sin(roll)), math.floor(-i*angle_step_deg), 20, 1, col, 0)
 
     end
 
